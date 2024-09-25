b78efdb8e34a9c1afb90a9d91670811308d72764
TiagoNelvo <ti.609@hotmail.com>
2024-09-25 17:40:39 -0300

    Exercicio

--- Exercicio 9 - Pesquisa.py
+++ Exercicio 9 - Pesquisa.py
@@ -1,4 +1,4 @@
-texto =  "Minha casa fica na rua Carneiro, 78. O CEP é 88388-000 e meu site é https://iaexpert.academy"
+texto =  "Minha casa fica na rua Carneiro, 78. O CEP é 88388-000 e meu site é https://iaexpert.academy http://iaexpert.academy"
 
 import re
 
@@ -6,4 +6,4 @@ re.findall(r'\d', texto)
 
 re.findall(r'\d{5}-\d{3}', texto)
 
-re.findall(r'https://[A-Za-z0-9./]+', texto)
+re.findall(r'https?://[A-Za-z0-9./]+', texto)
