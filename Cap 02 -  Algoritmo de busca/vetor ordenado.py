543ca8ab807c7fcf7cb29f89dee2f89a2201784b
TiagoNelvo <ti.609@hotmail.com>
2024-09-26 17:32:13 -0300

    Vetor Ordenado

--- Cap 02 -  Algoritmo de busca/vetor ordenado.py
+++ Cap 02 -  Algoritmo de busca/vetor ordenado.py
@@ -8,9 +8,51 @@ class VetorOrdenado:
         self.valores = np.empty(self.capacidade, dtype = int)
 
 #0(n)
-def imprime(self):
-    if self.ultima_posicao == -1:
-        print('O vetor esta vazio')
-    else:
+    def imprime(self):
+        if self.ultima_posicao == -1:
+            print('O vetor esta vazio')
+        else:
+            for i in range(self.ultima_posicao + 1):
+                print(i, ' - ', self.valores[i])
+            
+    def insere(self, valor):
+        if self.ultima_posicao == self.capacidade -1:
+            print('Capacidade máxima atingida')
+            return
+        
+        posicao = 0
         for i in range(self.ultima_posicao + 1):
-            print(i, ' - ', self.valores[i])
+            posicao = i
+            if self.valores[i] > valor:
+                break
+            if i == self.ultima_posicao:
+                posicao = i +1
+            
+        x = self.ultima_posicao
+        while x >= posicao:
+            self.valores[x + 1] = self.valores[x]
+            x -= 1
+            
+        self.valores[posicao] = valor
+        self.ultima_posicao += 1
+        
+vetor = VetorOrdenado(10)
+vetor.imprime()
+
+vetor.insere(6)
+vetor.imprime()
+
+vetor.insere(4)
+vetor.imprime()
+
+vetor.insere(3)
+vetor.imprime()
+
+vetor.insere(5)
+vetor.imprime()
+
+vetor.insere(1)
+vetor.imprime()
+
+vetor.insere(8)
+vetor.imprime()
