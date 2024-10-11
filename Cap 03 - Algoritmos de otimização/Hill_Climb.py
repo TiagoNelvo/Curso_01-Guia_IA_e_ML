90e88fc01e3c2ba11c03e49c53ff38b8747c973b
TiagoNelvo <ti.609@hotmail.com>
2024-10-11 19:16:38 -0300

    Hill Climb

---
+++ Cap 03 - Algoritmos de otimização/Hill_Climb.py
@@ -0,0 +1,12 @@
+import Voos
+from Voos import *
+
+#Hill Climb
+melhor_solucao, melhor_custo = mlrose.hill_climb(problema, random_state=1)
+melhor_solucao, melhor_custo
+
+print('-----------')
+imprimir_voos(melhor_solucao)
+print('-----------')
+
+voos[('BRU', 'FCO')]
--- Cap 03 - Algoritmos de otimização/Voos.py
+++ Cap 03 - Algoritmos de otimização/Voos.py
@@ -55,3 +55,42 @@ def imprimir_voos(agenda):
     print('Preço total: ', total_preco)
     
 imprimir_voos(agenda)
+
+# Fitness Function
+
+def fitness_function(agenda):
+    id_voo = -1
+    total_preco = 0
+    for i in range(len(agenda) // 2):
+        origem = pessoas[i][1]
+        id_voo += 1
+        ida = voos[(origem, destino)][agenda[id_voo]]
+        total_preco += ida[2]
+        id_voo += 1
+        volta = voos[(destino, origem)][agenda[id_voo]]
+        total_preco += volta[2]
+    return total_preco
+
+#fitness_function(agenda)
+
+
+import six
+import sys
+sys.modules['sklearn.externals.six'] = six
+import mlrose
+
+fitness = mlrose.CustomFitness(fitness_function)
+
+problema = mlrose.DiscreteOpt(length=12, fitness_fn=fitness,
+                              maximize = False, max_val=10)
+
+
+#Hill Climb
+melhor_solucao, melhor_custo = mlrose.hill_climb(problema, random_state=1)
+melhor_solucao, melhor_custo
+
+print('-----------')
+imprimir_voos(melhor_solucao)
+print('-----------')
+
+voos[('BRU', 'FCO')]
