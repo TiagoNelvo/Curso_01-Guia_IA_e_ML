87abad61c6655611a34ee4657e3364d7369b63d3
TiagoNelvo <ti.609@hotmail.com>
2024-10-11 16:17:55 -0300

    Voos

--- Cap 03 - Algoritmos de otimização/Voos.py
+++ Cap 03 - Algoritmos de otimização/Voos.py
@@ -34,3 +34,24 @@ voos[('LIS', 'FCO')]
 
 voos [('FCO', 'LIS')]
 
+agenda = [1,2, 3,2, 7,3, 6,3, 2,4, 5,3]
+#len(agenda)
+
+def imprimir_voos(agenda):
+    id_voo = -1
+    total_preco = 0
+    for i in range(len(agenda) // 2):
+        nome = pessoas[i][0]
+        origem = pessoas [i][1]
+        id_voo += 1
+        ida = voos[(origem, destino)][agenda[id_voo]]
+        total_preco += ida[2]
+        id_voo += 1
+        volta = voos[(destino, origem)][agenda[id_voo]]
+        total_preco += volta[2]
+        print('%10s%10s %5s-%5s %3s %5s-%5s %3s' % (nome, origem, ida[0], ida[1], ida[2],
+                                                    volta[0], volta[1], volta[2]))        
+        
+    print('Preço total: ', total_preco)
+    
+imprimir_voos(agenda)
