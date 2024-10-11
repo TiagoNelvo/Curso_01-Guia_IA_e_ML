6c1b44b8dfca8df0105d53bd45b1c3168e80fddf
TiagoNelvo <ti.609@hotmail.com>
2024-10-11 19:29:18 -0300

    Simulated Annealing

--- Cap 03 - Algoritmos de otimização/Simulated annealing.py
+++ Cap 03 - Algoritmos de otimização/Simulated annealing.py
@@ -1,3 +1,9 @@
 import Voos
 from Voos import *
 
+melhor_solucao, melhor_custo = mlrose.simulated_annealing(problema, 
+                                                          schedule=mlrose.decay.GeomDecay(init_temp=10000),
+                                                          random_state=0)
+melhor_solucao, melhor_custo
+
+imprimir_voos(melhor_solucao)
